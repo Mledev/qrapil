97e0b34ed4e018b1dca9ab8b98d5e43616c71da9
Matthieu <le.devehat.matth@gmail.com>
2018-04-27 12:01:35 +0200

    login api and refresh token

--- src/Controller/API/UserController.php
+++ src/Controller/API/UserController.php
@@ -6,20 +6,58 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 /**
- * @Route("/api/login")
+ * @Route("/api")
  */
 class UserController extends Controller
 {
     /**
-     * @Route("/", name="user_api_api", methods="GET")
+     * @Route("/login", name="user_login", methods="POST")
      */
-    public function api(): Response
+    public function login(Request $request, UserRepository $userRepository): Response
     {
-        return $this->json(['user'=>'Hello world']);
+        $users = $userRepository->findAll();
+        $connexion = $request->getContent();
+        $token = 0;
+        $connexion = json_decode($connexion, false , 2);
+
+        foreach ($users as $user) {
+            if ($connexion->{'Email'} === $user->getEmail()){
+                if($connexion->{'Password'} === $user->getPassword()){
+                    $token = $user->getToken();
+                }
+            }
+        }
+
+        return new JsonResponse(array('token' => $token));
+    }
+
+    /**
+     * @Route("/refreshToken", name="refresh_token", methods="POST")
+     */
+    public function refreshToken(Request $request, UserRepository $userRepository): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $users = $userRepository->findAll();
+        $connexion = $request->getContent();
+        $token = json_decode($connexion, false , 2);
+
+        foreach ($users as $user) {
+            if ($token->{'Token'} === $user->getToken()){
+                $newToken = $random = random_bytes(20);
+                $newToken = bin2hex($newToken);
+                $user->setToken($newToken);
+            }
+        }
+
+        $entityManager->flush();
+
+        return new JsonResponse(array('Token' => $newToken));
     }
 }
